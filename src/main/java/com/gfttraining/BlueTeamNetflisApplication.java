d4664387cd409a1f32895a6c89d9959e5c9c2a56
pauyarza <yarzapau@gmail.com>
2022-10-21 11:17:31 +0000

    first commit

---
+++ src/main/java/com/gfttraining/BlueTeamNetflisApplication.java
@@ -0,0 +1,13 @@
+package com.gfttraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueTeamNetflisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueTeamNetflisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfttraining/BlueTeamNetflisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfttraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueTeamNetflisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
