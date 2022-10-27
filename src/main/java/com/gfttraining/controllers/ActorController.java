72eb81368e0d7cbd2850284c8de2dc2816033468
pauyarza <yarzapau@gmail.com>
2022-10-27 14:29:43 +0000

    exceptions try

--- src/main/java/com/gfttraining/controllers/ActorController.java
+++ src/main/java/com/gfttraining/controllers/ActorController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,30 +26,38 @@ public class ActorController {
 	private ActorService actorService;
 	
 	@GetMapping ("api/actors")	
-	public List<Actor> allActors (){		
-		
-		return actorService.findAllActors();
+	ResponseEntity<List<Actor>> allActors (){
+		return new ResponseEntity<>(actorService.findAllActors(), HttpStatus.OK);
 	}
 	
 	@GetMapping ("api/actors/pages")	
-	public Page<Actor> allPageableActors(@PageableDefault(size = 10, page = 0) Pageable pageable){
-		
-		return actorService.findAllPageableActors(pageable);
+	public ResponseEntity<Page<Actor>> allPageableActors(@PageableDefault(size = 10, page = 0) Pageable pageable){
+		try {
+			return new ResponseEntity<>(actorService.findAllPageableActors(pageable), HttpStatus.OK);
+		} catch(Exception e) {
+			return new ResponseEntity<>(actorService.findAllPageableActors(pageable), HttpStatus.NOT_FOUND);
+		}
 	}
 	
+
 	@GetMapping ("/api/actors/{id}")
-	public Actor getActorrById(@PathVariable Integer id) {
-		
-		Actor actor= actorService.findOneActor(id);			
-			
-		return actor;				
-		
-	}		
+	ResponseEntity<Actor> getActorrById(@PathVariable Integer id) {
+		Actor actor= actorService.findOneActor(id);
+		try {
+			return new ResponseEntity<>(actor, HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<>(actor, HttpStatus.NOT_FOUND);
+		}
+	    
+	} 
 		
 	@PostMapping ("/api/actors")
-	public Actor postActor(@RequestBody Actor Actor ) {
-			
-		return actorService.newActor(Actor);
+	public ResponseEntity<Actor> postActor(@RequestBody Actor Actor ) {
+		try {
+			return new ResponseEntity<>(actorService.newActor(Actor), HttpStatus.CREATED);
+		}catch(Exception e) {
+			return new ResponseEntity<>(actorService.newActor(Actor), HttpStatus.NOT_FOUND);
+		}
 	}
 	
 	@PutMapping("/api/actors/{id}")
